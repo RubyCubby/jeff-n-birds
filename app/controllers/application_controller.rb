528d39139340537721f28874d554db3e6b71b58d
atjeff <ownerejeff@gmail.com>
2016-10-29 11:08:22 -0400

    Static pages, test cases, erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def testHello
+    render html: "Hello TestWorld"
+  end
+  
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @sample_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@sample_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@sample_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@sample_title}"
+  end
+
+end
