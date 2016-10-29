59d83cc91852d4465cc2d7aac226ead13f618de1
atjeff <ownerejeff@gmail.com>
2016-10-29 17:04:57 -0400

    Finish layout, added css, bootstrap, learned scss, testing beyond title, layout testing, routes, new users controller + tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,29 +5,29 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @sample_title = "Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get root" do
-    get root_url
-    assert_response :success
-    assert_select "title", "#{@sample_title}"
-  end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@sample_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@sample_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@sample_title}"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | #{@sample_title}"
   end
 
 end
