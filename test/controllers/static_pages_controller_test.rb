b8e0e6e44d21bbfa94d5779a1f6c27cb2674daa1
atjeff <ownerejeff@gmail.com>
2016-10-29 12:04:36 -0400

    Added helper function for page title, if not home add page name to title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@sample_title}"
+    assert_select "title", "#{@sample_title}"
   end
 
   test "should get about" do
